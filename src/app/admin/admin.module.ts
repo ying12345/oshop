ed501c7ae9912fadf2fe98147fbdc11d0dcf6c1a
Ying <guyingw@yahoo.com>
2019-04-05 15:44:51 -0500

    Adding icons. The icons are from https://fontawesome.com/v4.7.0/icons/

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ProductFormComponent } from './components/product-form/product-form.component';
-import { AdminProductsComponent } from './components/admin-products/admin-products.component';
-import { AdminOrdersComponent } from './components/admin-orders/admin-orders.component';
-import { AdminAuthGuard } from './services/admin-auth-guard.service';
-import { FormsModule } from '../../../node_modules/@angular/forms';
-import { SharedModule } from 'shared/shared.module';
-import { DataTableModule } from '../../../node_modules/angular-6-datatable';
 import { RouterModule } from '@angular/router';
 import { AuthGuard } from 'shared/services/auth-guard.service';
+import { SharedModule } from 'shared/shared.module';
+
+import { AdminOrdersComponent } from './components/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './components/admin-products/admin-products.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
+import { AdminAuthGuard } from './services/admin-auth-guard.service';
 
 @NgModule({
   declarations: [
